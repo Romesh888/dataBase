858b3115a6a3bc1742a79593b03127f16ad70127
Romesh888 <romesh.manilall888@gmail.com>
2021-06-22 21:34:36 +0530

    First Commit

---
+++ dataBase/src/main/java/com/gitproject/dataBase/dataBase/DataBaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gitproject.dataBase.dataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseProjectApplication.class, args);
+	}
+
+}
---
+++ dataBase/src/test/java/com/gitproject/dataBase/dataBase/DataBaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitproject.dataBase.dataBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
